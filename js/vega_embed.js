5d87dfed2d32d2194471ea99416c43d837054a04
bunhudavid <dbun0008@student.monash.edu>
2026-05-31 19:25:52 +1000

    Add files via upload

---
+++ js/vega_embed.js
@@ -0,0 +1,47 @@
+vegaEmbed("#kpi_cards", "specs/01_kpi_cards.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#venue_map", "specs/02_venue_map.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#club_attendance_bar", "specs/03_club_attendance_bar.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#venue_lollipop", "specs/04_venue_lollipop.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#round_attendance_line", "specs/05_round_attendance_line.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#home_away_comparison", "specs/06_home_away_comparison.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#rank_vs_crowd_scatter", "specs/07a_rank_vs_crowd_scatter.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#wins_vs_attendance_bubble", "specs/07b_wins_vs_attendance_bubble.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#venue_distribution_boxplot", "specs/08_venue_distribution_boxplot.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#club_report_cards", "specs/09_club_report_cards.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#crowd_heat_tiles", "specs/09b_crowd_heat_tiles.json", {
+  actions: false,
+}).catch(console.error);
+
+vegaEmbed("#final_summary_chart", "specs/10_final_summary_chart.json", {
+  actions: false,
+}).catch(console.error);
